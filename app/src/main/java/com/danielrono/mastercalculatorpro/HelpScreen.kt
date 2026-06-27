3a2ecfb369036ae1958088560a8552b431ce944b
aexaydace333-oss <aexaydace333@gmail.com>
2026-06-27 14:57:30 +0300

    Delete app/src/main/java/com/danielrono/mastercalculatorpro/HelpScreen.kt

--- app/src/main/java/com/danielrono/mastercalculatorpro/HelpScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.danielrono.mastercalculatorpro
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HelpScreen(){
-Text("HELP MANUAL\nScientific\nStatistics\nFinance\nGeometry")
-}
